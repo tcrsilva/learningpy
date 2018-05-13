86c23fd7e4c5894612a27f3b24e2229420dd9218
Tais Silva <tais.crsilva@gmail.com>
2018-05-13 01:17:44 +0000

    first commit

---
+++ one.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+print('Olá, Mundo!') #mostra mensagem "/', podendo usar qualquer caractere
+print(7+4) #faz a soma: 11
+print('7'+'4') #aparece um ao lado do outro: 74
+print ('Olá', 5) #aparece um ao lado do outro tb
+
+#o que for variável, sempre em letras minúsculas; 
+
+nome = 'Tais'
+idade = 19
+peso = 68,5
+print(nome,idade,peso)
+
+nome2 = input('Qual é o seu nome?')
+idade2 = input('Quantos anos você tem?')
+peso2 = input('Qual o seu peso?')
+
